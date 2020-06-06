374d0d11643390701804ea8c9a4c50701f9ecc2b
Rohit Moni <rohitmdxb@gmail.com>
2020-06-06 15:31:00 -0400

    Adding robots.txt route to stop crawlers

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,12 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Stop crawlers from seeing our webpage
+app.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: /");
+});
+
 var distPath = path.join(__dirname, '../../dist');
 console.log(distPath);
 app.use(express.static(distPath));
