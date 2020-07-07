3ba397af4d4461a2705685e27158c3ab28a0d26d
Rohit Moni <rohitmdxb@gmail.com>
2020-07-06 23:23:29 -0400

    Added lobby controller component

--- src/client/app.js
+++ src/client/app.js
@@ -1,11 +1,14 @@
 import './scss/app.scss';
 import React from "react";
 import ReactDOM from "react-dom";
+import LobbyController from "./components/lobby-controller";
 
 class App extends React.Component {
     render() {
         return (
-            <div id="main">Test 1234</div>
+            <div id="main">
+                <LobbyController />
+            </div>
         );
     }
 }
---
+++ src/client/components/lobby-controller.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class LobbyController extends React.Component {
+    render() {
+        return (
+            <div>1234</div>
+        )
+    }
+}
+
+export default LobbyController;
