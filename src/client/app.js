032ec68bf190566bde1f003cf1de676dfdbfa89b
Rohit Moni <rohitmdxb@gmail.com>
2020-07-07 22:27:59 -0400

    Starting to set up lobby controller with State

--- src/client/app.js
+++ src/client/app.js
@@ -3,14 +3,13 @@ import 'regenerator-runtime/runtime'; // Need this to solve a weird parcel bug
 import React from "react";
 import ReactDOM from "react-dom";
 import LobbyController from "./components/lobby-controller/lobby-controller";
-import TestStrikeDieMultiplayer from "../game/strikedie/game-component";
 
 class App extends React.Component {
     render() {
         return (
-            <div class="main">
+            <div className="main">
                 <h1>Strike-Die</h1>
-                <TestStrikeDieMultiplayer />
+                <LobbyController />
             </div>
         );
     }
--- src/client/components/lobby-controller/lobby-controller.js
+++ src/client/components/lobby-controller/lobby-controller.js
@@ -1,14 +1,60 @@
 import './lobby-controller.scss';
 import React from "react";
 
+var lobbyStates = {
+    NONE: 'none',
+    CREATED: 'hosting',
+    JOINED: 'joined',
+}
+
 class LobbyController extends React.Component {
+    static defaultProps = {
+        'currentState': lobbyStates.NONE,
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentState: props.currentState,
+        }
+    }
+
     render() {
-        return (
-            <div class="lobby-controller">
-                <button class="btn btn-primary">Create Game</button>
-                <button class="btn btn-primary">Join Game</button>
-            </div>
-        )
+        switch (this.state.currentState) {
+            case lobbyStates.NONE:
+                return (
+                    <div className="lobby-controller">
+                        <button className="btn btn-primary" onClick={this.onCreateGameClicked}>Create Game</button>
+                        <button className="btn btn-primary" onClick={this.onJoinGameClicked}>Join Game</button>
+                    </div>
+                )
+            case lobbyStates.CREATED:
+                return (
+                    <div className="lobby-controller">
+                        <p>Game Created</p>
+                        <button className="btn btn-primary" onClick={this.onBackClicked}>Back</button>
+                    </div>
+                )
+            case lobbyStates.JOINED:
+                return (
+                    <div className="lobby-controller">
+                        <p>Game Joined</p>
+                        <button className="btn btn-primary" onClick={this.onBackClicked}>Back</button>
+                    </div>
+                )
+        }
+    }
+
+    onCreateGameClicked = () => {
+        this.setState({currentState: lobbyStates.CREATED});
+    }
+
+    onJoinGameClicked = () => {
+        this.setState({ currentState: lobbyStates.JOINED });
+    }
+
+    onBackClicked = () => {
+        this.setState({ currentState: lobbyStates.NONE });
     }
 }
 
